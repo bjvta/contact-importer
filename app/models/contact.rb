837a56dcc0c54087f68a1a1a04d70159607a47d6
Brandon Valle <bjvtamayo78@gmail.com>
2023-03-14 13:08:55 -0400

    add the birthday format validator to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,7 @@ class Contact < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, name_format: true
-  validates :birthday, presence: true
+  validates :birthday, presence: true, birthday_format: true
   validates :phone, presence: true
   validates :credit_card_number, presence: true
   validates :credit_card_network, presence: true
