76c4bb4d20d1fbedae3284c87665c82184129057
Brandon Jason <bjvtamayo78@gmail.com>
2023-03-14 23:02:38 -0400

    feat: get the card network

--- app/services/get_card_network_service.rb
+++ app/services/get_card_network_service.rb
@@ -1,17 +1,27 @@
 module GetCardNetworkService
   AMERICAN_EXPRESS_REGEX = /^3[47][0-9]{13}$/
-  DINNERS_CLUB_REGEX = /^3[47][0-9]{13}$/
+  DINERS_CLUB_REGEX = /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/
+  DISCOVER_REGEX = /^6(?:011|5[0-9]{2})[0-9]{12}$/
+  JCB_REGEX = /^(?:2131|1800|35[0-9]{3})[0-9]{11}$/
+  MASTER_CARD_REGEX = /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/
+  VISA_REGEX = /^4[0-9]{12}(?:[0-9]{3})$/
 
   AMERICAN_EXPRESS = 'American Express'
-  DINNERS_CLUB = 'Dinners Club'
+  DINERS_CLUB = 'Diners Club'
+  DISCOVER = 'Discover'
+  JCB = 'JCB'
+  MASTER_CARD = 'MasterCard'
+  VISA = 'Visa'
 
   def self.call card_number
-    card_number = card_number.strip
-    if card_number.match AMERICAN_EXPRESS_REGEX
-      return AMERICAN_EXPRESS
-    end
-    if card_number.match DINNERS_CLUB_REGEX
-      return DINNERS_CLUB
-    end
+    card_number = card_number.gsub(' ', '')
+    return AMERICAN_EXPRESS if card_number.match AMERICAN_EXPRESS_REGEX
+    return DINERS_CLUB if card_number.match DINERS_CLUB_REGEX
+    return DISCOVER if card_number.match DISCOVER_REGEX
+    return JCB if card_number.match JCB_REGEX
+    return MASTER_CARD if card_number.match MASTER_CARD_REGEX
+    return VISA if card_number.match VISA_REGEX
+
+    'Invalid Card Number'
   end
 end
